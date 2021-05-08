a575dfed9d0797af280f458b78b9bad5d1b13e4e
Piotr Rydwański <pioryd123@gmail.com>
2021-05-08 19:10:36 +0200

    Add Interactable behavior

---
+++ src/behaviors/Interactable.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import { GameObjectApi, useGameObject } from "../managers/GameObject";
+
+import { Api } from "../core/useApi";
+import { EventData } from "../core/useEventManager";
+
+export type EventOnInteraction = EventData<"OnInteraction", GameObjectApi>;
+
+export type InteractableApi = Api<
+  "Interactable",
+  {
+    interactWith: (gameObjectApi: GameObjectApi) => Promise<boolean>;
+    onInteractFrom: (gameObjectApi: GameObjectApi) => Promise<void>;
+  }
+>;
+
+export default function Interactable() {
+  const gameObjectApi = useGameObject();
+  const { eventManager: eventManagerOfGameObject, apiManager } = gameObjectApi;
+
+  React.useLayoutEffect(() => {
+    return apiManager.add<InteractableApi>("Interactable", {
+      async interactWith(gameObjectApi) {
+        const interactableApi = gameObjectApi.apiManager.get<InteractableApi>(
+          "Interactable"
+        );
+
+        if (!interactableApi) return false;
+
+        await interactableApi.onInteractFrom(gameObjectApi);
+
+        return true;
+      },
+      async onInteractFrom(gameObject) {
+        await eventManagerOfGameObject.emit<EventOnInteraction>(
+          "OnInteraction",
+          gameObject
+        );
+      }
+    });
+  }, []);
+
+  return null;
+}
